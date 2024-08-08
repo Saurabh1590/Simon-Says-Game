ea27f13922cd9d39c6de3e2b381325b9f562c5fe
Saurabh1590 <148901127+Saurabh1590@users.noreply.github.com>
2024-08-08 18:57:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["yellow", "red", "purple", "green"];
+
+let started = false;
+let level = 0;
+let highestScore = localStorage.getItem("highestScore") || 0; // Retrieve highest score from local storage
+
+let h2 = document.querySelector("h2");
+let h3 = document.querySelector("h3");
+let h4 = document.createElement("h4"); // Create an element for the highest score
+h4.innerText = `Highest Score: ${highestScore}`; // Display the highest score
+document.body.insertBefore(h4, h3); // Insert h4 before h3
+
+document.addEventListener("keypress", function() {
+    if (!started) {
+        console.log("game is started");
+        started = true;
+        levelUp();
+    }
+});
+
+function gameFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 250);
+}
+
+function userFlash(btn) {
+    btn.classList.add("userflash");
+    setTimeout(function () {
+        btn.classList.remove("userflash");
+    }, 250);
+}
+
+function levelUp() {
+    userSeq = [];
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * btns.length);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    gameFlash(randBtn);
+}
+
+function checkAns(idx) {
+    if (userSeq[idx] === gameSeq[idx]) {
+        if (userSeq.length == gameSeq.length) {
+            setTimeout(levelUp, 1000);
+        }
+    } else {
+        if (level > highestScore) { // Check if the current score is the highest score
+            highestScore = level;
+            localStorage.setItem("highestScore", highestScore); // Store the highest score in local storage
+            h4.innerText = `Highest Score: ${highestScore}`; // Display the highest score
+        }
+        h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br> Press any key to start.`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function() {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+
+        reset();
+    }
+}
+
+function btnPress() {
+    let btn = this;
+    userFlash(btn);
+
+    let userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length - 1);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for (let btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
